2469927434fa29d9914e231984a492699ba70f2f
raishabirah <raihanshabirah20@gmail.com>
2023-10-04 15:07:41 +0700

    fix: index

--- indexCorpus.py
+++ indexCorpus.py
@@ -4,17 +4,19 @@ import nltk
 from nltk.corpus import stopwords
 from Sastrawi.Stemmer.StemmerFactory import StemmerFactory
 
-nltk.download('punkt')
-nltk.download('stopwords')
+# nltk.download('punkt')
+# nltk.download('stopwords')
 
-def index_txt_files(input_folder, output_file):
+def index_txt_files(input_folder, output_file, index_file):
     # Inisialisasi stemmer dan daftar stopwords
     factory = StemmerFactory()
     stemmer = factory.create_stemmer()
     stop_words = set(stopwords.words('indonesian'))
 
+    word_index = {}  # Kamus untuk menyimpan indeks kata
+
     with open(output_file, 'w', encoding='utf-8') as outfile:
-        for filename in os.listdir(input_folder):
+        for file_num, filename in enumerate(os.listdir(input_folder), start=1):
             if filename.endswith('.txt'):
                 file_path = os.path.join(input_folder, filename)
                 with open(file_path, 'r', encoding='utf-8') as infile:
@@ -33,10 +35,27 @@ def index_txt_files(input_folder, output_file):
                     # Tulis token ke file output
                     outfile.write(' '.join(tokens) + '\n')
 
+                    # Perbarui indeks kata
+                    for i, token in enumerate(tokens):
+                        if token not in word_index:
+                            word_index[token] = {file_num: [i]}
+                        else:
+                            if file_num not in word_index[token]:
+                                word_index[token][file_num] = [i]
+                            else:
+                                word_index[token][file_num].append(i)
+
+    # Tulis indeks kata ke file dalam bentuk list
+    with open(index_file, 'w', encoding='utf-8') as indexfile:
+        for word, indices in word_index.items():
+            index_list = [[file_num, index] for file_num, index in indices.items()]  # Buat list dari kamus
+            indexfile.write(f'{word}: {index_list}\n')
+
 def main():
     input_folder = 'corpus'
-    output_file = 'index.txt'
-    index_txt_files(input_folder, output_file)
+    output_file = 'tokenized.txt'
+    index_file = 'indexWord.txt'
+    index_txt_files(input_folder, output_file, index_file)
     print('Indeks file teks selesai.')
 
 if __name__ == "__main__":
